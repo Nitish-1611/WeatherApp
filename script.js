7a774567a1e93d3733165441399c2a0db4d3ee5d
Nitish-1611 <91827367+Nitish-1611@users.noreply.github.com>
2023-11-26 15:15:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+apiKey = "607942deb607657269ada418523128fb";
+apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon")
+
+async function checkWeather(city) {
+    const response = await fetch(apiUrl + city + "&appid=" + apiKey);
+    if (response.status == 404) {
+        alert("Invalid City")
+        document.querySelector(".weather").style.display = "none"
+    }
+    else {
+        var data = await response.json();
+
+        document.querySelector(".city").innerHTML = data.name;
+        document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+        document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+        document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+
+        if (data.weather[0].main == "Clouds") {
+            weatherIcon.src = "images/clouds.png"
+        }
+        else if (data.weather[0].main == "Clear") {
+            weatherIcon.src = "images/clouds.png"
+        }
+        else if (data.weather[0].main == "Rain") {
+            weatherIcon.src = "images/rain.png"
+        }
+        else if (data.weather[0].main == "Drizzle") {
+            weatherIcon.src = "images/drizzle.png"
+        }
+        else if (data.weather[0].main == "Mist") {
+            weatherIcon.src = "images/mist.png"
+        }
+        else if (data.weather[0].main == "Snow") {
+            weatherIcon.src = "images/snow.png"
+        }
+
+        document.querySelector(".weather").style.display = "block"
+    }
+}
+
+searchBtn.addEventListener("click", () => {
+    checkWeather(searchBox.value);
+})
+
+function clickPress(event) {
+    if (event.keyCode == 13) {
+        checkWeather(searchBox.value);
+    }
+}
+
+
+//console.log(apiUrl + `&appid=${apiKey}`);
